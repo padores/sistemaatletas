6288ac25fddda9c0d6e4fc3b2d43b59d69a3e17e
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-02 10:00:48 -0300

    Criação da classe pai Atleta

---
+++ Atleta.java
@@ -0,0 +1,72 @@
+package sistemaatletas;
+
+import java.util.Date;
+
+public class Atleta {
+	
+	private String nome;
+	private String sobrenome;
+	private Date dataDeNascimento;
+	private String nomeDaMae;
+	private String cpf;
+	private char sexo;
+	private double altura;
+	private double peso;
+	
+	public Atleta(String nome, char sexo){
+		this.nome = nome;
+		this.sexo = sexo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public String getNomeDaMae() {
+		return nomeDaMae;
+	}
+	public void setNomeDaMae(String nomeDaMae) {
+		this.nomeDaMae = nomeDaMae;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	
+
+}
