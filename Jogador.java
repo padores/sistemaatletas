360649f3f3e3d4ff7a2e77483cf9c984efce4aad
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-02 09:56:07 -0300

    Complementando a classe Jogador e fazendo relaçoes com a classe Time

---
+++ Jogador.java
@@ -0,0 +1,12 @@
+package sistemaatletas;
+
+public class Jogador extends Atleta {
+
+	private String numero;
+	private String posicao;
+	
+	public Jogador(String nome, char sexo) {
+		super(nome, sexo);
+	}
+
+}
