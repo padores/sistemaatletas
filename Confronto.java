0522a5cd1d8170edb5b2b624f124150268e500e9
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-04 03:05:26 -0300

    Alteração no sistema de confronto

--- Confronto.java
+++ Confronto.java
@@ -4,41 +4,39 @@ public class Confronto {
 	
 	private Time time1;
 	private Time time2;
-	private String nomeConfronto;
 	
 	public Confronto(Time time1, Time time2){
 		this.time1 = time1;
 		this.time2 = time2;
+
 	}
 	
 	public void resultadoConfronto(int scoreTime1, int scoreTime2){
+		int v1, v2, d1, d2, e1, e2;
+		v1=time1.getVitorias();
+		v2=time2.getVitorias();
+		d1=time1.getDerrotas();
+		d2=time2.getDerrotas();
+		e1=time1.getEmpates();
+		e2=time2.getEmpates();
+		
 		if(scoreTime1>scoreTime2){
-			time1.setVitorias(time1.getVitorias()+1);
-			time2.setVitorias(time2.getDerrotas()+1);
+			time1.setVitorias(v1+1);
+			time2.setDerrotas(d2+1);
 			System.out.println("O time vencedor foi: "+time1.getNomeTime());
-			time1.adicionar(getNomeConfronto());
 		}
 		
 		else if(scoreTime1<scoreTime2){
-			time2.setVitorias(time2.getVitorias()+1);
-			time1.setVitorias(time1.getDerrotas()+1);
+			time2.setVitorias(v2+1);
+			time1.setDerrotas(d1+1);
 			System.out.println("O time vencedor foi: "+time2.getNomeTime());
-			time2.adicionar(getNomeConfronto());
 		}
 		
 		else{
-			time1.setEmpates(time1.getEmpates()+1);
-			time2.setEmpates(time2.getEmpates()+1);
+			time1.setEmpates(e1+1);
+			time2.setEmpates(e2+1);
 			System.out.println("Empate!");
 		}
-	}
-
-	public String getNomeConfronto() {
-		return nomeConfronto;
-	}
-
-	public void setNomeConfronto(String nomeConfronto) {
-		this.nomeConfronto = nomeConfronto;
 	}
 
 	
