1f1c03bc20349cf0703d9f6a603f00e45d63f7b0
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-09 12:42:26 -0300

    Arrumando o sistema de confrontos

--- Confronto.java
+++ Confronto.java
@@ -12,29 +12,37 @@ public class Confronto {
 	}
 	
 	public void resultadoConfronto(int scoreTime1, int scoreTime2){
-		int v1, v2, d1, d2, e1, e2;
+		int v1, v2, d1, d2, e1, e2, n1, n2;
 		v1=time1.getVitorias();
 		v2=time2.getVitorias();
 		d1=time1.getDerrotas();
 		d2=time2.getDerrotas();
 		e1=time1.getEmpates();
 		e2=time2.getEmpates();
+		n1=time1.getNumeroDeJogos();
+		n2=time2.getNumeroDeJogos();
 		
 		if(scoreTime1>scoreTime2){
 			time1.setVitorias(v1+1);
 			time2.setDerrotas(d2+1);
+			time1.setNumeroDeJogos(n1+1);
+			time2.setNumeroDeJogos(n2+1);
 			System.out.println("O time vencedor foi: "+time1.getNomeTime());
 		}
 		
 		else if(scoreTime1<scoreTime2){
 			time2.setVitorias(v2+1);
 			time1.setDerrotas(d1+1);
+			time1.setNumeroDeJogos(n1+1);
+			time2.setNumeroDeJogos(n2+1);
 			System.out.println("O time vencedor foi: "+time2.getNomeTime());
 		}
 		
 		else{
 			time1.setEmpates(e1+1);
 			time2.setEmpates(e2+1);
+			time1.setNumeroDeJogos(n1+1);
+			time2.setNumeroDeJogos(n2+1);
 			System.out.println("Empate!");
 		}
 	}
