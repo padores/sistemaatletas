c796f273e2736732d169fa4341dddb1d9f193801
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-09 13:54:07 -0300

    Atualizando a classe

--- Main.java
+++ Main.java
@@ -6,24 +6,22 @@ public class Main {
 		
 		Time T1 = new Time("Heks", "N arena");
 		Time T2 = new Time("Brons", "Y arena");
-		Jogador J1 = new Jogador("Jon",'H', T1);
-		Jogador B1 = new Jogador("Ken", 'H', T2);
+		Jogador J1 = new Jogador("Jon",'H');
+		Jogador B1 = new Jogador("Ken", 'H');
 		Confronto Con1 = new Confronto(T1, T2);
 		
 		T1.adicionar(J1);
 		T1.adicionar(B1);
-		T1.remover(J1);
-		T2.adicionar(J1);
-		System.out.println(T1.getListaJogadores());
-		System.out.println(T2.getListaJogadores());
+		T1.getListaJogadores();
 		T1.listaStatusTime();
-		T2.listaStatusTime();
-		Con1.resultadoConfronto(7, 14);
-		Con1.resultadoConfronto(44, 21);
 		Con1.resultadoConfronto(7, 7);
+		Con1.resultadoConfronto(9, 18);
+		Con1.resultadoConfronto(18, 16);
 		T1.listaStatusTime();
-		T2.listaStatusTime();
+		T1.getListaJogadores();
 		
+		
+
 	}
 
 }
