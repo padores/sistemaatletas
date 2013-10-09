6c7d29bea5ac4575f132109575225c9c6731bd60
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-09 13:53:49 -0300

    Atualizando a classe

--- Confronto.java
+++ Confronto.java
@@ -12,7 +12,7 @@ public class Confronto {
 	}
 	
 	public void resultadoConfronto(int scoreTime1, int scoreTime2){
-		int v1, v2, d1, d2, e1, e2, n1, n2;
+		int i, i2, v1, v2, d1, d2, e1, e2, n1, n2;
 		v1=time1.getVitorias();
 		v2=time2.getVitorias();
 		d1=time1.getDerrotas();
@@ -21,12 +21,25 @@ public class Confronto {
 		e2=time2.getEmpates();
 		n1=time1.getNumeroDeJogos();
 		n2=time2.getNumeroDeJogos();
+		i=0;
+		i2=0;
 		
 		if(scoreTime1>scoreTime2){
 			time1.setVitorias(v1+1);
 			time2.setDerrotas(d2+1);
 			time1.setNumeroDeJogos(n1+1);
 			time2.setNumeroDeJogos(n2+1);
+			
+			while(i < time1.listaJogadores.size()){
+				time1.listaJogadores.get(i).setVitorias(+1);
+				i++;
+			}
+			
+			while(i2 < time2.listaJogadores.size()){
+					time2.listaJogadores.get(i2).setDerrotas(+1);
+					i++;
+			}
+			
 			System.out.println("O time vencedor foi: "+time1.getNomeTime());
 		}
 		
@@ -35,7 +48,16 @@ public class Confronto {
 			time1.setDerrotas(d1+1);
 			time1.setNumeroDeJogos(n1+1);
 			time2.setNumeroDeJogos(n2+1);
+			while(i < time1.listaJogadores.size()){
+				time1.listaJogadores.get(i).setDerrotas(+1);
+				i++;
+			}
+			while(i2 < time2.listaJogadores.size()){
+					time2.listaJogadores.get(i2).setVitorias(+1);
+					i++;
+			}
 			System.out.println("O time vencedor foi: "+time2.getNomeTime());
+			
 		}
 		
 		else{
@@ -43,6 +65,14 @@ public class Confronto {
 			time2.setEmpates(e2+1);
 			time1.setNumeroDeJogos(n1+1);
 			time2.setNumeroDeJogos(n2+1);
+			while(i < time1.listaJogadores.size()){
+				time1.listaJogadores.get(i).setEmpates(+1);
+				i++;
+			}
+			while(i2 < time2.listaJogadores.size()){
+					time2.listaJogadores.get(i2).setEmpates(+1);
+					i++;
+			}	
 			System.out.println("Empate!");
 		}
 	}
