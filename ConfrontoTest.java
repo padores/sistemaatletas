b3216de93f7e04fd79711bfca20b7cd21a967305
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-09 21:07:10 -0300

    Adicionando testes para adição de resultado da partida para jogadores

--- ConfrontoTest.java
+++ ConfrontoTest.java
@@ -9,11 +9,14 @@ public class ConfrontoTest {
 
 	Confronto umConfronto;
 	Time aTime, bTime; 
+	Jogador aJogador;
 	
 	@Before
 	public void setUp() throws Exception{
+		aJogador = new Jogador("Don", 'H');
 		aTime = new Time("Bro", "MetLife");
 		bTime = new Time("Baha","Heinz");
+		aTime.adicionar(aJogador);
 		umConfronto = new Confronto(aTime, bTime);
 		umConfronto.resultadoConfronto(7, 14);
 		umConfronto.resultadoConfronto(14, 7);
@@ -24,18 +27,26 @@ public class ConfrontoTest {
 	public void testDerrotas() {
 		assertEquals(1, aTime.getDerrotas());
 		assertEquals(1, bTime.getDerrotas());
+		assertEquals(1, aJogador.getDerrotas());
 	}
 	
 	@Test
 	public void testVitorias() {
 		assertEquals(1, aTime.getVitorias());
 		assertEquals(1, bTime.getVitorias());
+		assertEquals(1, aJogador.getVitorias());
 	}
 	
 	@Test
 	public void testEmpates() {
 		assertEquals(1, aTime.getEmpates());
 		assertEquals(1, bTime.getEmpates());
+		assertEquals(1, aJogador.getEmpates());
 	}
-
+	
+	@Test
+	public void testTotalJogos(){
+		assertEquals(3, aTime.getNumeroDeJogos());
+		assertEquals(3, bTime.getNumeroDeJogos());
+	}
 }
